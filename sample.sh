c7e0ce35b87d05d07d2f902df502b9aa17ccacee
riteshalu <129961618+riteshalu@users.noreply.github.com>
2023-08-25 10:31:06 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,11 +1,1 @@
-echo "This is my first project"
-echo "This is my second change"
-echo "This is my third change from local git"
-echo "This is a change from Github"
-echo "This is a stash lab"
-echo "I am doing a lab for Branch"
-echo "Git Rebase"
-echo "I am in Git Branch br1"
-echo "Gir Rebase in branch"
-
-
+echo "This test file"
